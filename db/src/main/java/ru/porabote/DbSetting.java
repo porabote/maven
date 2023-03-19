a662261e3fd17bfe09a5df8583bee0294a9ed454
maksimov <dmaksimov@vtb.ru>
2023-03-19 13:23:52 +0300

    task1

---
+++ db/src/main/java/ru/porabote/DbSetting.java
@@ -0,0 +1,13 @@
+package ru.porabote;
+
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/ru/porabote/Main.java
@@ -0,0 +1,7 @@
+package ru.porabote;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
